14d06f20090595a9fe0b29e29131028094d1e6da
Samuel Peña <samuel_pena@hotmail.es>
2019-03-02 10:38:58 -0400

    tercer commit con unit test

---
+++ solAritmetica/UTaritmetica/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UTaritmetica")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UTaritmetica")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8622bdb1-446d-440e-89b5-9c9e5af829e5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ solAritmetica/UTaritmetica/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UTaritmetica
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //arrange
+            const int numa = 1;
+            const int numb = 2;
+            const int resp = 3;
+            //act
+            using (srvAritmetica.WSaritmeticaClient cliente = new srvAritmetica.WSaritmeticaClient())
+            {
+                var rsp = cliente.sumar(numa, numb);
+
+                //assert
+                Assert.AreEqual(resp, rsp.respuesta);
+            }
+            //assert
+        }
+
+     }
+}
